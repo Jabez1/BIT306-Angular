963d63b0573cc611367c049368330d3676089994
Muhammad <Muhammad@example.com>
2023-04-15 14:28:21 +0800

    --

--- src/app/emp-schedule/rev-emp-schedule/rev-emp-schedule.component.ts
+++ src/app/emp-schedule/rev-emp-schedule/rev-emp-schedule.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { WorkHours } from '../emp-schedule.model';
+import { Subscription } from 'rxjs';
+import { DailySchedule } from '../emp-schedule.model';
+
+
 
 @Component({
   selector: 'app-rev-emp-schedule',
@@ -6,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./rev-emp-schedule.component.css']
 })
 export class RevEmpScheduleComponent {
+  readonly WorkHours = WorkHours;
+  private empSchedList: Subscription | undefined;
+
+  objValues = Object.values;
+  objKeys = Object.keys;
+
+  empSched: DailySchedule[] = [];
+
+  groupedEmpWorkHours: any = [];
+
+  
 
 }
